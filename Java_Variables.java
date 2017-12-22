5de0d0223363f1480c6a803fde3b4be57e1688d5
vishwas lodhi <30628763+vklp1234@users.noreply.github.com>
2017-12-22 18:20:32 +0000

    different type of datatypes used in java

---
+++ Java_Variables.java
@@ -0,0 +1,22 @@
+//package manager;
+public class Java_Variables
+{
+	public static void main(String []args)
+	{
+		byte a1 = 10;
+		short a2 = 10;
+		int a3 = 10;
+		long a4 = 10;
+		float a5 = (float)10.85;
+		double a6 = (double)1282.80;
+		char a7 = 'v';
+		String a8 = "vishwsa lodhi";
+		boolean a9 = true;
+		System.out.println("byte = "+a1+"\n short = "+a2+"\n int = "+a3+"\n long = "+a4+"\n float = "+a5+"\n double = "+a6+"\n char = "+a7+"\n String = "+a8+"\n boolean = "+a9);
+	
+	
+	
+	}
+
+
+}
